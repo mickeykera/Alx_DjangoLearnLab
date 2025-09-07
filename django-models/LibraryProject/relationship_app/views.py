0a4cd1e847d772fd4754dbaef952166116938083
Mike <mickeykera7@gmail.com>
2025-09-07 13:04:13 +0300

    New files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Library
 from .models import Book
-
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 def book_list_text(request):
