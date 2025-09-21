151565fcd177560564cc7ddf24742892434352f9
Mike <mickeykera7@gmail.com>
2025-09-21 20:26:35 +0300

    API

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -1,12 +1,35 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    """
+    List all books - requires authentication to view
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    CRUD operations for books - requires authentication for all operations
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
